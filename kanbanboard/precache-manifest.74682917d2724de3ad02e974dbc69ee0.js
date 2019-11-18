70e8e1af71c926334a5ebce22caf3796b30810d8
Ivan <ivanpejanic@outlook.com>
2019-11-18 17:47:45 +0100

    kanbanboard

---
+++ kanbanboard/precache-manifest.74682917d2724de3ad02e974dbc69ee0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a82b69cb22eb93283868e634f939fbe0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "88cb885cbf388508dbf7",
+    "url": "./static/css/main.04556b1f.chunk.css"
+  },
+  {
+    "revision": "5dbf847c8470b45a0529",
+    "url": "./static/js/2.414b4813.chunk.js"
+  },
+  {
+    "revision": "88cb885cbf388508dbf7",
+    "url": "./static/js/main.c9ac3f03.chunk.js"
+  },
+  {
+    "revision": "284ad9111fbe99530a72",
+    "url": "./static/js/runtime-main.51c78183.js"
+  }
+]);
