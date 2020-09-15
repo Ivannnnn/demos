728d2d5a8a6302f71a07cb73234be9475384e4ab
Ivannnnn <ipejanic@gmail.com>
2020-09-15 12:20:15 +0200

    Add files via upload

---
+++ task-tracker/precache-manifest.8ca1618ef7ab09024d7bf647a82c1209.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "933b044a1d8f62374bea66a6cef57e25",
+    "url": "./index.html"
+  },
+  {
+    "revision": "42c8e34e61bfb5c7f755",
+    "url": "./static/css/main.c53fdaea.chunk.css"
+  },
+  {
+    "revision": "fafebb12e21b36585ba9",
+    "url": "./static/js/2.78ca21d8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.78ca21d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42c8e34e61bfb5c7f755",
+    "url": "./static/js/main.3310ffef.chunk.js"
+  },
+  {
+    "revision": "4b1c863f650443891fc9",
+    "url": "./static/js/runtime-main.38dd7e8d.js"
+  }
+]);
